fc66bbe0602b2cc74ba5975e370bec21105f399d
Musiker15 <info@musiker15.de>
2024-06-04 21:42:09 +0200

    Update

--- server/server.lua
+++ server/server.lua
@@ -252,8 +252,11 @@ GithubUpdater = function()
 
     if Config.VersionChecker then
         PerformHttpRequest('https://raw.githubusercontent.com/Musiker15/VERSIONS/main/WeaponAmmo.json', function(errorCode, jsonString, headers)
-            print("###############################")
-			if not jsonString then print(resourceName .. '^1Update Check failed! ^3Please Update to the latest Version: ^9https://keymaster.fivem.net/^0') print("###############################") return end
+			if not jsonString then 
+                print(resourceName .. '^1Update Check failed ^3Please Update to the latest Version:^9 https://github.com/MSK-Scripts/msk_weaponammo ^0')
+                print(resourceName .. '^2 ✓ Resource loaded^0 - ^5Current Version: ^0' .. CurrentVersion)
+                return
+            end
 
 			local decoded = json.decode(jsonString)
             local version = decoded[1].version
@@ -262,14 +265,14 @@ GithubUpdater = function()
                 print(resourceName .. '^2 ✓ Resource is Up to Date^0 - ^5Current Version: ^2' .. CurrentVersion .. '^0')
             elseif CurrentVersion ~= version then
                 print(resourceName .. '^1 ✗ Resource Outdated. Please Update!^0 - ^5Current Version: ^1' .. CurrentVersion .. '^0')
-                print('^5Latest Version: ^2' .. version .. '^0 - ^6Download here: ^9https://keymaster.fivem.net/^0')
+                print('^5Latest Version: ^2' .. version .. '^0 - ^6Download here:^9 https://github.com/MSK-Scripts/msk_weaponammo/releases/tag/v'.. version .. '^0')
 				print('')
 				if not string.find(CurrentVersion, 'beta') then
 					for i=1, #decoded do 
 						if decoded[i]['version'] == CurrentVersion then
 							break
 						elseif not isVersionIncluded(decoded, CurrentVersion) then
-							print('^1You are using the ^3BETA VERSION^1 of ^0' .. resourceName)
+							print('^1You are using an^3 UNSUPPORTED VERSION^1 of ^0' .. resourceName)
 							break
 						end
 
@@ -282,15 +285,12 @@ GithubUpdater = function()
 						end
 					end
 				else
-					print('^1You are using the ^3BETA VERSION^1 of ^0' .. resourceName)
+					print('^1You are using the^3 BETA VERSION^1 of ^0' .. resourceName)
 				end
             end
-            print("###############################")
         end)
     else
-        print("###############################")
-        print(resourceName .. '^2 ✓ Resource loaded^0')
-        print("###############################")
+        print(resourceName .. '^2 ✓ Resource loaded^0 - ^5Current Version: ^2' .. CurrentVersion .. '^0')
     end
 end
 GithubUpdater()
